f9e9896ac0e93af2baa86e276d3699fbb430274d
Babatunde Lamidi <odunayohlamidi@gmail.com>
2020-06-15 23:54:18 +0100

    javascript done

---
+++ app.js
@@ -0,0 +1,21 @@
+const container = document.querySelector('.container');
+const text = document.querySelector('#text');
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 2;
+
+breatheAnimation()
+
+function breatheAnimation() {
+  text.innerHTML = 'Breathe In!';
+  container.className = 'container grow';
+  setTimeout(() => {
+    text.innerText = 'Hold';
+    setTimeout(() => {
+      text.innerText = 'Breathe Out!';
+      container.className = 'Container Shrink';
+    }, holdTime);
+  }, breatheTime);
+}
+
+setInterval(breatheAnimation, totalTime)
